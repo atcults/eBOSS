8f9c7500bf9084fe99d76e0d095039aec8a933f8
Sunny Rajwadi <sunny@sanelib.com>
2015-12-17 13:19:57 +0530

    Updating Jersey config for package scan.

--- api/src/main/java/org/sanelib/eboss/api/services/JerseyConfig.java
+++ api/src/main/java/org/sanelib/eboss/api/services/JerseyConfig.java
@@ -1,16 +1,12 @@
 package org.sanelib.eboss.api.services;
 
 import org.glassfish.jersey.server.ResourceConfig;
-import org.sanelib.eboss.api.services.employee.EmployeeService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
-        //packages("org.sanelib.eboss.api.services");
-        register(HelloService.class);
-        register(EmployeeService.class);
-
+        packages("org.sanelib.eboss.api.services");
     }
 }
