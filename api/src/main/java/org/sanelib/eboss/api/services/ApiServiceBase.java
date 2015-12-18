86262b5c543f2dd2cae8aa9adf76493ff36345e5
Sunny Rajwadi <sunny@sanelib.com>
2015-12-18 11:46:22 +0530

    Modify ApiServiceBase for Transaction support.

--- api/src/main/java/org/sanelib/eboss/api/services/ApiServiceBase.java
+++ api/src/main/java/org/sanelib/eboss/api/services/ApiServiceBase.java
@@ -2,13 +2,15 @@ package org.sanelib.eboss.api.services;
 
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.sanelib.eboss.api.dto.BaseDTO;
+import org.sanelib.eboss.core.dao.UnitOfWork;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class ApiServiceBase {
+public abstract class ApiServiceBase {
 
     @Autowired
     RuntimeService runtimeService;
@@ -16,10 +18,23 @@ public class ApiServiceBase {
     @Autowired
     TaskService taskService;
 
-    public void execute(BaseDTO dto, String processKey) throws Exception {
+    @Autowired
+    UnitOfWork unitOfWork;
+
+    protected void execute(BaseDTO dto, String processKey) {
         Map<String, Object> variables = new HashMap<>();
         variables.put("dto", dto);
-        runtimeService.startProcessInstanceByKey(processKey, variables);
+
+        try {
+            unitOfWork.begin();
+            ProcessInstance instance = runtimeService.startProcessInstanceByKey(processKey, variables);
+            System.out.println(instance.getId());
+            unitOfWork.commit();
+        } catch (Exception exception){
+            System.out.println(exception.getMessage());
+            unitOfWork.rollback();
+        }
+
     }
 
 }
