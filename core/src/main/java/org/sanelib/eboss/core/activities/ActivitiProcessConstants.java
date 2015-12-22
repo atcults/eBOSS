6ca858b8136bccce0a5f4d5c1545cd6aadc21fdc
Sunny Rajwadi <sunny@sanelib.com>
2015-12-22 09:48:45 +0530

    Adding missing file from last commit.

---
+++ core/src/main/java/org/sanelib/eboss/core/activities/ActivitiProcessConstants.java
@@ -0,0 +1,9 @@
+package org.sanelib.eboss.core.activities;
+
+public class ActivitiProcessConstants {
+
+    public class Admin{
+        public static final String ADD_EMPLOYEE_PROCESS = "addEmployeeProcess";
+    }
+
+}
