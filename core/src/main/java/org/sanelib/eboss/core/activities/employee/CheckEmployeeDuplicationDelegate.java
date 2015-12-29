1bf69842b70a81388e37c8be059ef80d294b6a75
Sunny Rajwadi <sunny@sanelib.com>
2015-12-29 16:03:23 +0530

    Applying check for duplication of employee code and employee mail address.

--- core/src/main/java/org/sanelib/eboss/core/activities/employee/CheckEmployeeDuplicationDelegate.java
+++ core/src/main/java/org/sanelib/eboss/core/activities/employee/CheckEmployeeDuplicationDelegate.java
@@ -2,13 +2,56 @@ package org.sanelib.eboss.core.activities.employee;
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import org.sanelib.eboss.core.commands.employee.AddEmployee;
+import org.sanelib.eboss.core.commands.employee.UpdateEmployee;
+import org.sanelib.eboss.core.dao.EmployeeRepository;
+import org.sanelib.eboss.core.domain.entity.Employee;
+import org.sanelib.eboss.core.exceptions.AppException;
+import org.sanelib.eboss.core.exceptions.ProcessError;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @Component
 public class CheckEmployeeDuplicationDelegate implements JavaDelegate {
 
-	@Override
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    ProcessError processError;
+
+    @Override
 	public void execute(DelegateExecution execution) throws Exception {
 		System.out.println("Checking employee for duplication");
-	}
+
+        Object command = execution.getVariable("command");
+
+        Integer existingEmployeeId = (command instanceof UpdateEmployee) ? ((UpdateEmployee) command).getId() : null;
+
+        //Code duplication check
+        String employeeCode = ((AddEmployee) command).getCode();
+
+        List<Employee> employees = employeeRepository.findByColumnAndValue("code", employeeCode);
+
+        if(!employees.isEmpty() && !Objects.equals(existingEmployeeId, employees.get(0).getId())){
+            processError.addError("common.field.duplicate", "code", Arrays.asList("domain.employee.name", "domain.employee.code"), employeeCode);
+        }
+
+        //Email duplication check
+        String emailAddress = ((AddEmployee) command).getEmail();
+
+        employees = employeeRepository.findByColumnAndValue("email", emailAddress);
+
+        if(!employees.isEmpty() && !Objects.equals(existingEmployeeId, employees.get(0).getId())){
+            processError.addError("common.field.duplicate", "email", Arrays.asList("domain.employee.name", "domain.employee.email"), emailAddress);
+        }
+
+        if(!processError.isValid()){
+            throw new AppException(processError);
+        }
+    }
 }
