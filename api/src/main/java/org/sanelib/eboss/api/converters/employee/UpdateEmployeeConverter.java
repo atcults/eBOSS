bd4e18892f03a55363b081afe8e0aba0dfd8c098
Sunny Rajwadi <sunny@sanelib.com>
2015-12-29 18:44:34 +0530

    Mapping id for update employee with validation.

--- api/src/main/java/org/sanelib/eboss/api/converters/employee/UpdateEmployeeConverter.java
+++ api/src/main/java/org/sanelib/eboss/api/converters/employee/UpdateEmployeeConverter.java
@@ -1,9 +1,7 @@
 package org.sanelib.eboss.api.converters.employee;
 
-import com.google.common.base.Strings;
-import org.sanelib.eboss.api.converters.DtoToCommandConverter;
 import org.sanelib.eboss.api.dto.employee.EmployeeDTO;
-import org.sanelib.eboss.common.utils.DateHelper;
+import org.sanelib.eboss.common.utils.RegularExpressionHelper;
 import org.sanelib.eboss.core.commands.ProcessCommand;
 import org.sanelib.eboss.core.commands.employee.UpdateEmployee;
 import org.sanelib.eboss.core.exceptions.ProcessError;
@@ -16,11 +14,16 @@ public class UpdateEmployeeConverter extends AddEmployeeConverter {
     @Autowired
     ProcessError processError;
 
-    //TODO: Need to validate number and convert id value
     @Override
     public ProcessCommand convert(EmployeeDTO dto) {
         UpdateEmployee command = (UpdateEmployee) super.convert(dto);
-        //command.setId(dto.getId());
+
+        if(RegularExpressionHelper.checkIdFormat(dto.getId())){
+            processError.addError("common.field.required", "id", "domain.employee.id");
+        } else {
+            command.setId(Integer.parseInt(dto.getId()));
+        }
+
         return command;
     }
 }
