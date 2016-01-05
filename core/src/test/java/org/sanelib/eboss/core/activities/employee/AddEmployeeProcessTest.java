b205bb903f1448962efe080b009cad67d81f53cf
Sunny Rajwadi <sunny@sanelib.com>
2016-01-05 21:19:04 +0530

    Updating AddEmployeeProcessTest for all properties.

--- core/src/test/java/org/sanelib/eboss/core/activities/employee/AddEmployeeProcessTest.java
+++ core/src/test/java/org/sanelib/eboss/core/activities/employee/AddEmployeeProcessTest.java
@@ -1,6 +1,7 @@
 package org.sanelib.eboss.core.activities.employee;
 
 import org.junit.Test;
+import org.sanelib.eboss.common.utils.DateHelper;
 import org.sanelib.eboss.common.utils.RegularExpressionHelper;
 import org.sanelib.eboss.BaseSpringJUnitTest;
 import org.sanelib.eboss.core.activities.ActivitiProcessConstants;
@@ -13,13 +14,23 @@ public class AddEmployeeProcessTest extends BaseSpringJUnitTest {
 
 	@Test
 	public void testAddEmployeeProcessTest() throws Throwable {
-        AddEmployee addEmployee= new AddEmployee();
-        addEmployee.setCode("EMP001");
-        addEmployee.setFirstName("First Name");
-        addEmployee.setLastName("Last Name");
+        AddEmployee addEmployee = new AddEmployee();
+        addEmployee.setCode("101");
+        addEmployee.setFirstName("fName");
+        addEmployee.setMiddleName("mName");
+        addEmployee.setLastName("lName");
+        addEmployee.setDateOfBirth(DateHelper.constructDate(1990, 1, 1));
+        addEmployee.setAddressLine1("7,Shakti Park Soc");
+        addEmployee.setAddressLine2("College Road");
+        addEmployee.setCity("Nadiad");
+        addEmployee.setState("Gujarat");
         addEmployee.setCountry("India");
+        addEmployee.setZipCode("387001");
         addEmployee.setPhone("+91-9876543210");
-        addEmployee.setEmail("name@yahoo.com");
+        addEmployee.setEmail("fname@gmail.com");
+        addEmployee.setGender("Male");
+        addEmployee.setDateOfJoining(DateHelper.constructDate(2015, 7 ,1));
+        addEmployee.setIsActive(true);
 
         String result = execute(addEmployee, ActivitiProcessConstants.Admin.ADD_EMPLOYEE);
 
@@ -32,7 +43,22 @@ public class AddEmployeeProcessTest extends BaseSpringJUnitTest {
 
         assertNotNull(employee);
 
-        assertEquals(addEmployee.getCode(), addEmployee.getCode());
+        assertEquals(addEmployee.getCode(), employee.getCode());
+        assertEquals(addEmployee.getFirstName(),employee.getFirstName());
+        assertEquals(addEmployee.getMiddleName(),employee.getMiddleName());
+        assertEquals(addEmployee.getLastName(),employee.getLastName());
+        assertEquals(addEmployee.getDateOfBirth(),employee.getDateOfBirth());
+        assertEquals(addEmployee.getAddressLine1(),employee.getAddressLine1());
+        assertEquals(addEmployee.getAddressLine2(),employee.getAddressLine2());
+        assertEquals(addEmployee.getCity(),employee.getCity());
+        assertEquals(addEmployee.getState(),employee.getState());
+        assertEquals(addEmployee.getCountry(),employee.getCountry());
+        assertEquals(addEmployee.getZipCode(),employee.getZipCode());
+        assertEquals(addEmployee.getPhone(),employee.getPhone());
+        assertEquals(addEmployee.getEmail(),employee.getEmail());
+        assertEquals(addEmployee.getGender(),employee.getGender());
+        assertEquals(addEmployee.getDateOfJoining(),employee.getDateOfJoining());
+        assertEquals(addEmployee.getIsActive(),employee.getIsActive());
 
     }
 }
