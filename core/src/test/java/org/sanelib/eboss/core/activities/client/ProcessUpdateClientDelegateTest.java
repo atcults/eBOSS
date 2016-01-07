66c05324f85d9137b4fc03096ac313796078172c
piusanelib <disha.sil@sanelib.com>
2016-01-07 10:59:32 +0530

    Adding UpdateClientDelegate process, Delegate Test and UpdateClient process Test

---
+++ core/src/test/java/org/sanelib/eboss/core/activities/client/ProcessUpdateClientDelegateTest.java
@@ -0,0 +1,50 @@
+package org.sanelib.eboss.core.activities.client;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.sanelib.eboss.common.utils.DateHelper;
+import org.sanelib.eboss.core.activities.employee.ProcessUpdateEmployeeDelegate;
+import org.sanelib.eboss.core.commands.client.UpdateClient;
+import org.sanelib.eboss.core.commands.employee.UpdateEmployee;
+import org.sanelib.eboss.core.dao.ClientRepository;
+import org.sanelib.eboss.core.dao.EmployeeRepository;
+import org.sanelib.eboss.core.domain.entity.Client;
+import org.sanelib.eboss.core.domain.entity.Employee;
+
+public class ProcessUpdateClientDelegateTest {
+
+    ClientRepository clientRepositoryMock;
+
+    ProcessUpdateClientDelegate processUpdateClientDelegate;
+
+    @Before
+    public void setUp(){
+        clientRepositoryMock = Mockito.mock(ClientRepository.class);
+        processUpdateClientDelegate = new ProcessUpdateClientDelegate();
+        processUpdateClientDelegate.clientRepository = clientRepositoryMock;
+    }
+
+    @Test
+    public void testAddClientSuccessExecute() throws Exception {
+
+        Mockito.doNothing().when(clientRepositoryMock).save(Mockito.isA(Client.class));
+
+        UpdateClient updateClient = new UpdateClient();
+        updateClient.setClientId("new123");
+        updateClient.setClientSecret("NewSecret");
+        updateClient.setClientName("NewWEB");
+        updateClient.setDescription("NewDescription");
+        updateClient.setClientUrl("NewUrl");
+        updateClient.setClientType("NewType");
+        updateClient.setScope("NewScope");
+        updateClient.setRedirectUri("NewRedirect");
+        updateClient.setRegdate(DateHelper.fromDateString("2006/02/02"));
+
+        DelegateExecution execution = Mockito.mock(DelegateExecution.class);
+        Mockito.when(execution.getVariable("command")).thenReturn(updateClient);
+
+      }
+
+}
---
+++ core/src/test/java/org/sanelib/eboss/core/activities/client/UpdateClientProcessTest.java
@@ -0,0 +1,63 @@
+package org.sanelib.eboss.core.activities.client;
+
+
+import org.junit.Test;
+import org.sanelib.eboss.BaseSpringJUnitTest;
+import org.sanelib.eboss.common.utils.DateHelper;
+import org.sanelib.eboss.core.activities.ActivitiProcessConstants;
+import org.sanelib.eboss.core.commands.client.UpdateClient;
+import org.sanelib.eboss.core.domain.entity.Client;
+
+
+import static org.junit.Assert.*;
+
+public class UpdateClientProcessTest extends BaseSpringJUnitTest {
+
+    @Test
+    public void testUpdateClientProcessTest() throws Throwable {
+
+        Client client = new Client();
+        client.setClientId("123");
+        client.setClientSecret("secret");
+        client.setClientName("WEB");
+        client.setDescription("Description");
+        client.setClientUrl("url");
+        client.setClientType("type");
+        client.setScope("Scope");
+        client.setRedirectUri("redirect");
+        client.setRegdate(DateHelper.fromDateString("2005/04/01"));
+
+        persist(client);
+
+        UpdateClient updateClient = new UpdateClient();
+        updateClient.setId(client.getId());
+        updateClient.setClientId("new123");
+        updateClient.setClientSecret("NewSecret");
+        updateClient.setClientName("NewWEB");
+        updateClient.setDescription("NewDescription");
+        updateClient.setClientUrl("NewUrl");
+        updateClient.setClientType("NewType");
+        updateClient.setScope("NewScope");
+        updateClient.setRedirectUri("NewRedirect");
+        updateClient.setRegdate(DateHelper.fromDateString("2006/02/02"));
+
+        String result = execute(updateClient, ActivitiProcessConstants.Admin.UPDATE_CLIENT);
+
+        assertNull(result);
+
+        assertNotNull(updateClient);
+
+        assertEquals("new123", updateClient.getClientId());
+        assertEquals("NewSecret",updateClient.getClientSecret());
+        assertEquals("NewWEB",updateClient.getClientName());
+        assertEquals("NewDescription",updateClient.getDescription());
+        assertEquals("NewUrl",updateClient.getClientUrl());
+        assertEquals("NewType",updateClient.getClientType());
+        assertEquals("NewScope",updateClient.getScope());
+        assertEquals("NewRedirect",updateClient.getRedirectUri());
+        assertEquals("2006/02/02",DateHelper.toDateString(updateClient.getRegdate()));
+
+
+    }
+
+}
