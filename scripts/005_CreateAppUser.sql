09f742e6602e38c6eb04630a5906803c3fb5f909
amrita8791 <amrita.gianchandani@sanelib.com>
2016-01-07 12:48:47 +0530

    Adding User Creation Script.

---
+++ scripts/005_CreateAppUser.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `app_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
