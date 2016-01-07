5ad84c18bb3e4e395f436633a3743f22d36bf7df
piusanelib <disha.sil@sanelib.com>
2016-01-07 10:46:18 +0530

    Updating ApiEndPointConstants

--- api/src/main/java/org/sanelib/eboss/api/services/ApiEndPointConstants.java
+++ api/src/main/java/org/sanelib/eboss/api/services/ApiEndPointConstants.java
@@ -5,6 +5,7 @@ public class ApiEndPointConstants {
     public class Admin{
         public static final String EMPLOYEE_END_POINT = "/employee";
         public static final String CUSTOMER_END_POINT = "/customer";
+        public static final String CLIENT_END_POINT = "/client";
     }
 
 }
