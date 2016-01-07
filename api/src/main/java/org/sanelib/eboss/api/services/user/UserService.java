402a75b567d16fe1f40e2aec93e78d60938cb5ff
amrita8791 <amrita.gianchandani@sanelib.com>
2016-01-07 12:41:29 +0530

    Updating UserService.

--- api/src/main/java/org/sanelib/eboss/api/services/user/UserService.java
+++ api/src/main/java/org/sanelib/eboss/api/services/user/UserService.java
@@ -13,6 +13,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,7 +36,6 @@ public class UserService extends ApiServiceBase {
 
         UserDTO userDTO = new UserDTO();
 
-        userDTO.setId("001");
         userDTO.setUsername("firstUser");
         userDTO.setPassword("password");
 
@@ -47,5 +47,10 @@ public class UserService extends ApiServiceBase {
     @POST
     public String addUser(UserDTO userDTO) throws Throwable {
         return execute(userDTO, ActivitiProcessConstants.Admin.ADD_USER);
+    }
+
+    @PUT
+    public String updateUser(UserDTO userDTO) throws Throwable {
+        return execute(userDTO, ActivitiProcessConstants.Admin.UPDATE_USER);
     }
 }
