3f98c8f823b5b72fb3fe2d782d4fa0cd906e0ca4
Sunny Rajwadi <sunny@sanelib.com>
2016-01-09 20:29:21 +0530

    Refactoring classes.

--- core/src/test/java/org/sanelib/eboss/core/activities/customer/UpdateCustomerProcessTest.java
+++ core/src/test/java/org/sanelib/eboss/core/activities/customer/UpdateCustomerProcessTest.java
@@ -3,7 +3,7 @@ package org.sanelib.eboss.core.activities.customer;
 
 import org.junit.Test;
 import org.sanelib.eboss.common.utils.RegularExpressionHelper;
-import org.sanelib.eboss.BaseSpringJUnitTest;
+import org.sanelib.eboss.EntityIntegrationTestBase;
 import org.sanelib.eboss.core.activities.ActivitiProcessConstants;
 import org.sanelib.eboss.core.commands.customer.UpdateCustomer;
 import org.sanelib.eboss.core.domain.entity.Customer;
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-public class UpdateCustomerProcessTest extends BaseSpringJUnitTest {
+public class UpdateCustomerProcessTest extends EntityIntegrationTestBase {
     @Test
     public void testUpdateCustomerProcessTest() throws Throwable {
         UpdateCustomer update = new UpdateCustomer();
