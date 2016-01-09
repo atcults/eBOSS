eaa2d38cc3be2234b26ae2172532dc5775b541fd
Sunny Rajwadi <sunny@sanelib.com>
2016-01-09 20:31:40 +0530

    Modifying ViewService that can act as component and load data from database by using jdbc.

--- core/src/main/java/org/sanelib/eboss/core/service/ViewService.java
+++ core/src/main/java/org/sanelib/eboss/core/service/ViewService.java
@@ -1,14 +1,40 @@
 package org.sanelib.eboss.core.service;
 
 import org.sanelib.eboss.core.domain.view.DomainView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
-public interface ViewService<T extends DomainView> {
+@Component
+public class ViewService {
 
-	List<T> loadAll();
+    @Autowired
+    DataSource dataSource;
 
-	T load(long id);
+    public Connection getConnection() throws SQLException {
+        return this.dataSource.getConnection();
+    }
 
-	T get(long id);
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public <T extends DomainView> List<T> loadQuery(String query, ViewMapper mapper) {
+        final List<T> list = new ArrayList<>();
+
+        try (Statement stmt = getConnection().createStatement()) {
+            try (DataResultSet rs = new DataResultSet(stmt.executeQuery(query))) {
+                while (rs.next()) {
+                    list.add((T) mapper.map(rs));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 }
